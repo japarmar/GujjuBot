de9280165477e91fde44a14a7f31cd3cc8e41695
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2020-03-31 11:24:52 +0530

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -131,8 +131,14 @@ async def _(event):
                 while response.text == FILLED_UP_DADDY:
                     pack += 1
                     prevv = int(pack) - 1
-                    packname = f"{user.first_name}'s GujjuBot Vol.{pack}"
-                    packshortname = f"Vol._{pack}_with_{userid}"
+                    #packname = f"{user.first_name}'s GujjuBot Vol.{pack}"
+                    #packshortname = f"Vol._{pack}_with_{userid}"
+                    if userid == 948408212:
+                        packname = f"Vol_{pack}_JayukeStickers"
+                        packshortname = "Vol_{pack}_Jayu_ke_locker_me"
+                    else:
+                        packname = f"Vol_{pack}_GujjuBot{userid}"
+                        packshortname = f"Vol_{pack}_GujjuBot_{userid}_ns"
                     if not await stickerset_exists(bot_conv, packshortname):
                         await event.edit("**Pack No. **" + str(prevv) + "** full! Making a new Pack, Vol. **" + str(pack))
                         if is_a_s:
