991aad8c4fea50488e3b13f91c8ad7300c86cf5b
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2020-04-08 12:56:03 +0530

    Update carbon.py
    Carbon is fixed now. don't update your bot now. We will inform you for update the bot.

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -152,7 +152,7 @@ async def carbon_api(e):
 
          file,
 
-         caption="<< Here's your carbon, \n Carbonised by [GujjuBot](https://www.github.com/GujjuBot)>> ",
+         caption="<< Here's your carbon, \n Carbonised by [GujjuBot](https://www.github.com/japarmar/GujjuBot)>> ",
 
          force_document=True,
 
