4ebe10f8d034ff04aed169ec83620f8b53b03546
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2020-04-08 12:49:19 +0530

    Update figlet.py

--- userbot/plugins/figlet.py
+++ userbot/plugins/figlet.py
@@ -1,6 +1,8 @@
 import pyfiglet
+from userbot.utils import admin_cmd
 
-@command(pattern="^.figlet ?(.*)", outgoing=True)
+#@command(pattern="^.figlet ?(.*)", outgoing=True)
+@borg.on(admin_cmd(pattern=r"figlet ?(.*)"), , outgoing=True)
 async def figlet(event):
     if event.fwd_from:
         return
