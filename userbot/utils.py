242630a255cefb104928baa37985a28b3a5e57d1
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2020-04-23 18:27:55 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -129,9 +129,9 @@ def remove_plugin(shortname):
     except:
         raise ValueError
 
-def admin_cmd(**args):
-    pattern = args.get("pattern", None)
-    allow_sudo = args.get("allow_sudo", False)
+def admin_cmd(pattern=None, allow_sudo=False,**args):
+    #pattern = args.get("pattern", None)
+    #allow_sudo = args.get("allow_sudo", False)
 
     # get the pattern from the decorator
     if pattern is not None:
