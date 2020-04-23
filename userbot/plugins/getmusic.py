b31261a3fb6011a9fb2027ed5d4be67951bc325e
noobanon <51384033+noobanon@users.noreply.github.com>
2020-04-23 12:10:05 +0530

    sudo

--- userbot/plugins/getmusic.py
+++ userbot/plugins/getmusic.py
@@ -23,7 +23,7 @@ def bruh(name):
     os.system("instantmusic -q -s "+name)
     
 
-@borg.on(admin_cmd(pattern="song ?(.*)"))
+@borg.on(admin_cmd(pattern="song ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
