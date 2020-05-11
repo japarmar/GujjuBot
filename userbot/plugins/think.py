fe6b45ee983e688e4ae5c9375b94b6299a12b58e
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2020-05-11 14:42:14 +0530

    Update think.py

--- userbot/plugins/think.py
+++ userbot/plugins/think.py
@@ -17,7 +17,6 @@ async def _(event):
     animation_interval = 0.1
     animation_ttl = range(0, 288)
     
-    await event.edit(input_str)
     animation_chars = [
             "THINKING",
             "THI&K#N₹",
