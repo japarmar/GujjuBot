06e9e5bb48ee12d243266d0a233aecb486d79212
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2020-06-07 13:50:25 +0530

    Update cmd_list.py

--- userbot/plugins/cmd_list.py
+++ userbot/plugins/cmd_list.py
@@ -17,5 +17,5 @@ async def install(event):
     o = stdout.decode()
     _o = o.split("\n")
     o = "\n".join(_o)
-    OUTPUT = f"**List of Plugins:**\n{o}\n\n**TIP:** __If you want to know the commands for a plugin, do:-__ \n `.help <plugin name>` **without the < > brackets.**\n__All plugins might not work directly. Visit__ @Gujju_Bot_Support_1 __for assistance.__"
+    OUTPUT = f"**List of Plugins:**\n{o}\n\n**TIP:** __If you want to know the commands for a plugin, do:-__ \n `.help <plugin name>` **without the < > brackets.**\n__All plugins might not work directly. Visit__ @Gujju_Bot_Support1 __for assistance.__"
     await event.edit(OUTPUT)
