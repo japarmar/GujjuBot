396886691b9e2921189554ab170410a7389bc496
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2020-07-07 10:09:33 +0530

    Update listmyusernames.py

--- userbot/plugins/listmyusernames.py
+++ userbot/plugins/listmyusernames.py
@@ -16,11 +16,3 @@ async def mine(event):
     await event.edit(output_str)
 
     
-@borg.on(admin_cmd("sudolistmyusernames", allow_sudo=True))
-async def mine(event):
-    """ For .reserved command, get a list of your reserved usernames. """
-    result = await bot(GetAdminedPublicChannelsRequest())
-    output_str = ""
-    for channel_obj in result.chats:
-        output_str += f"{channel_obj.title}\n@{channel_obj.username}\n\n"
-    await event.edit(output_str)
