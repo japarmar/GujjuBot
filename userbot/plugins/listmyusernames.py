4449d60934e0629e23b4bbe951938bec88895bb7
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2020-07-07 09:54:33 +0530

    Update listmyusernames.py

--- userbot/plugins/listmyusernames.py
+++ userbot/plugins/listmyusernames.py
@@ -7,6 +7,15 @@ from uniborg.util import admin_cmd
 from telethon.tl.functions.channels import GetAdminedPublicChannelsRequest
 
 @borg.on(admin_cmd("listmyusernames"))
+async def mine(event):
+    """ For .reserved command, get a list of your reserved usernames. """
+    result = await bot(GetAdminedPublicChannelsRequest())
+    output_str = ""
+    for channel_obj in result.chats:
+        output_str += f"{channel_obj.title}\n@{channel_obj.username}\n\n"
+    await event.edit(output_str)
+
+    
 @borg.on(admin_cmd("sudolistmyusernames", allow_sudo=True))
 async def mine(event):
     """ For .reserved command, get a list of your reserved usernames. """
