da66e852e8d0df6119d1008ccf88a02cb7be8131
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2020-07-07 10:25:44 +0530

    Update snip.py

--- userbot/plugins/snip.py
+++ userbot/plugins/snip.py
@@ -81,7 +81,7 @@ async def on_snip_list(event):
             OUT_STR += f"👉 #{a_snip.snip} \n"
     else:
         OUT_STR = "No Snips. Start Saving using `.snips`"
-    if len(OUT_STR) > Config.MAX_MESSAGE_SIZE_LIMIT:
+    if len(OUT_STR) > 1024:
         with io.BytesIO(str.encode(OUT_STR)) as out_file:
             out_file.name = "snips.text"
             await borg.send_file(
