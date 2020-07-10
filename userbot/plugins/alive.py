c4c71d131546435400147767faceb40de8ed7bbf
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2020-07-10 16:04:12 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -15,7 +15,7 @@ async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit("`Jinda Hu Sarr ^.^ \nYour bot is running\n\nTelethon version: 6.9.0\nPython: 3.7.3\n\n`"
                      f"`My peru owner`: {DEFAULTUSER}\n"
-                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @Halto_Tha\n"
+                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @movies_company\n"
                      "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
                      "[Deploy this userbot Now](https://github.com/japarmar/GujjuBot)")
 
