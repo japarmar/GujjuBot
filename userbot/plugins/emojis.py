17b40ac7cc63fdd94c92e2a7d7e4008735522d76
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2021-02-12 12:35:19 +0530

    Update emojis.py

--- userbot/plugins/emojis.py
+++ userbot/plugins/emojis.py
@@ -38,3 +38,5 @@ async def _(event):
         for i in animation_ttl:
             await asyncio.sleep(animation_interval)
             await event.edit(animation_chars[i % 2])
+    else:
+        await event.edit("Please reply with `shrug` or `apple` or `:/` or `-_-`")
