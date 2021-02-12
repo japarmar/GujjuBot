5aab7897e5eacf106e9006bfab180d1db1f9b6d0
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2021-02-12 10:01:26 +0530

    Update pmto.py

--- userbot/plugins/pmto.py
+++ userbot/plugins/pmto.py
@@ -1,22 +1,33 @@
 from userbot.utils import admin_cmd
+from telethon.tl.functions.users import GetFullUserRequest
 
 
 @borg.on(admin_cmd(pattern="pmto ?(.*)"))
 async def pmto(event):
-    a = event.pattern_match.group(1)
-    b = a.split(" ")
-    chat_id = b[0]
-    try:
-        chat_id = int(chat_id)
-    except BaseException:
-        pass
-    msg = ""
-    for i in b[1:]:
-        msg += i + " "
-    if msg == "":
-        return
-    try:
-        await borg.send_message(chat_id, msg)
-        await event.edit("Message sent!")
-    except BaseException:
-        await event.edit("Something went wrong.")
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        chat_id=await event.client(GetFullUserRequest(reply_message.from_id))
+        msg = event.pattern_match.group(1)
+        try:
+            await borg.send_message(chat_id, msg)
+            await event.edit("Message sent!")
+        except BaseException:
+            await event.edit("Something went wrong.")
+    else:
+        a = event.pattern_match.group(1)
+        b = a.split(" ")
+        chat_id = b[0]
+        try:
+            chat_id = int(chat_id)
+        except BaseException:
+            pass
+        msg = ""
+        for i in b[1:]:
+            msg += i + " "
+        if msg == "":
+            return
+        try:
+            await borg.send_message(chat_id, msg)
+            await event.edit("Message sent!")
+        except BaseException:
+            await event.edit("Something went wrong.")
