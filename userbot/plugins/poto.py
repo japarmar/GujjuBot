4334e1cf8fc297359de7cf436c5dd5b7cc26ddf3
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2021-02-12 11:06:35 +0530

    Add files via upload

---
+++ userbot/plugins/poto.py
@@ -0,0 +1,42 @@
+import logging
+
+from uniborg.util import admin_cmd
+
+logger = logging.getLogger(__name__)
+
+
+if 1 == 1:
+    name = "Profile Photos"
+    client = borg
+
+    @borg.on(admin_cmd(pattern="poto(.*)"))
+    async def potocmd(event):
+        """Gets the profile photos of replied users, channels or chats"""
+        id = "".join(event.raw_text.split(maxsplit=2)[1:])
+        user = await event.get_reply_message()
+        chat = event.input_chat
+        if user:
+            photos = await event.client.get_profile_photos(user.sender)
+        else:
+            photos = await event.client.get_profile_photos(chat)
+        if id.strip() == "":
+            try:
+                await event.client.send_file(event.chat_id, photos)
+            except a:
+                photo = await event.client.download_profile_photo(chat)
+                await borg.send_file(event.chat_id, photo)
+        else:
+            try:
+                id = int(id)
+                if id <= 0:
+                    await event.edit("`ID number you entered is invalid`")
+                    return
+            except BaseException:
+                await event.edit("`Are you Comedy Me ?`")
+                return
+            if int(id) <= (len(photos)):
+                send_photos = await event.client.download_media(photos[id - 1])
+                await borg.send_file(event.chat_id, send_photos)
+            else:
+                await event.edit("`That user has no photos 🙄`")
+                return
