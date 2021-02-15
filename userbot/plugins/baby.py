5bca30b01e312aa84d9de395c69c0c70f8c61bb3
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2021-02-15 11:47:00 +0530

    Add files via upload

---
+++ userbot/plugins/baby.py
@@ -0,0 +1,185 @@
+# This plugin made by noob @none1p.. don't copy it plzz.. credit goes to @none1p
+# special thanks to  @YOU_ARE_UNDER_ARREST and @M_A_R_S_H_M_E_L_L_O_W for helping me
+from telethon import events
+import asyncio
+import os
+import sys
+from userbot.utils import admin_cmd
+import random
+from userbot import bot as borg
+img1=("https://telegra.ph/file/2479b98d08eed6308dfba.mp4")
+img2=("https://telegra.ph/file/f81f5ecbcb5ad7c07c093.mp4")
+img3=("https://telegra.ph/file/a8feefcdfc6adcda499bf.mp4")
+img4=("https://telegra.ph/file/c9b14ac778ffbae2b8877.mp4")
+img5=("https://telegra.ph/file/4be0c8224a6b49b796505.mp4")
+img6=("https://telegra.ph/file/ee2bb9d5c4c41751c0cd1.mp4")
+img7=("https://telegra.ph/file/e982bd34bc29b2908a218.mp4")
+img8=("https://telegra.ph/file/f7b2555d6e5c11cd94d8f.mp4")
+img9=("https://telegra.ph/file/30df9bd45b861fd0f5773.mp4")
+img10=("https://telegra.ph/file/f5364c94795991747f9a7.mp4")
+img11=("https://telegra.ph/file/227dc0c3315061e25a5b4.mp4")
+img12=("https://telegra.ph/file/c3f47d45cf14ceb0c70f4.mp4")
+img13=("https://telegra.ph/file/3d3e86283953a292aef4a.mp4")
+img14=("https://telegra.ph/file/6311a00f207a1702183e2.mp4")
+img15=("https://telegra.ph/file/6a561f28fa2e3151e19ce.mp4")
+img16=("https://telegra.ph/file/2e6647c61f188addc10d6.mp4")
+img0=("https://telegra.ph/file/7135eefe9b9958c240e3f.mp4")
+img17=("https://telegra.ph/file/c3fae40f56b09d14cd3ae.mp4")
+img18=("https://telegra.ph/file/9a91ffe0c5df545d6342a.mp4")
+img19=("https://telegra.ph/file/397bdb1c7464e31c412ab.mp4")
+img20=("https://telegra.ph/file/56826efd263c7b9a016a2.mp4")
+img21=("https://telegra.ph/file/7949c971080325e844efa.mp4")
+img22=("https://telegra.ph/file/63a513716c46ab6ace04a.mp4")
+img23=("https://telegra.ph/file/18e805a3dc055dfdf769d.mp4")
+img24=("https://telegra.ph/file/92a1d658ae45b254e7664.mp4")
+img25=("https://telegra.ph/file/3c9b0b695ec00e8f5cfba.mp4")
+img26=("https://telegra.ph/file/eff58c655413f69037e51.mp4")
+img27=("https://telegra.ph/file/1a5b2b0c6a984ffd411cc.mp4")
+img28=("https://telegra.ph/file/245512fedf2af3153f044.mp4")
+img29=("https://telegra.ph/file/2d0abdbb19d2b20cb408c.mp4")
+img30=("https://telegra.ph/file/fbce171bf949465ad718e.mp4")
+img31=("https://telegra.ph/file/61299e8dbaa60aa54bbf2.mp4")
+img32=("https://telegra.ph/file/ecbbf730117561217cb51.mp4")
+img33=("https://telegra.ph/file/03dfc6e846fcdac8fff7a.mp4")
+img34=("https://telegra.ph/file/9f20d26589b50532b5b58.mp4")
+img35=("https://telegra.ph/file/1acc616ff8b0b499edd1d.mp4")
+img36=("https://telegra.ph/file/7199340d207398afe8acc.mp4")
+img37=("https://telegra.ph/file/406c6cf22b6b84fb29eeb.mp4")
+img38=("https://telegra.ph/file/fe3cf0d2269cd6edab394.mp4")
+img39=("https://telegra.ph/file/f7b4f5813f063c5f8b3ed.mp4")
+img40=("https://telegra.ph/file/04cdec4e301a311b21584.mp4")
+    
+    
+@borg.on(admin_cmd(outgoing=True, pattern="baby"))
+
+async def _(event):
+
+    if event.fwd_from:
+        return
+    await event.edit("SENDING...:-)")
+    await asyncio.sleep(0.9)
+    x=(random.randrange(0,40))
+    if x==1:
+        await borg.send_file(event.chat_id,img1)
+        await event.delete()
+    if x==2:
+        await borg.send_file(event.chat_id,img2)
+        await event.delete()
+    if x==3:
+        await borg.send_file(event.chat_id,img3)
+        await event.delete()
+    if x==4:
+        await borg.send_file(event.chat_id,img3)
+        await event.delete()        
+    if x==5:
+        await borg.send_file(event.chat_id,img4)
+        await event.delete()
+    if x==6:
+        await borg.send_file(event.chat_id,img5)
+        await event.delete()
+    if x==7:
+        await borg.send_file(event.chat_id,img6)
+        await event.delete()
+    if x==8:
+        await borg.send_file(event.chat_id,img7)
+        await event.delete()        
+    if x==9:
+        await borg.send_file(event.chat_id,img9)
+        await event.delete()        
+    if x==10:
+        await borg.send_file(event.chat_id,img10)
+        await event.delete()
+    if x==11:
+        await borg.send_file(event.chat_id,img11)
+        await event.delete()
+    if x==12:
+        await borg.send_file(event.chat_id,img12)
+        await event.delete()
+    if x==13:
+        await borg.send_file(event.chat_id,img13)
+        await event.delete()         
+    if x==14:
+        await borg.send_file(event.chat_id,img14)
+        await event.delete()
+    if x==15:
+        await borg.send_file(event.chat_id,img15)
+        await event.delete()
+    if x==16.:
+        await borg.send_file(event.chat_id,img16)
+        await event.delete()
+    if x==0:
+        await borg.send_file(event.chat_id,img0)
+        await event.delete()
+    if x==17:
+        await borg.send_file(event.chat_id,img17)
+        await event.delete()
+    if x==18:
+        await borg.send_file(event.chat_id,img18)
+        await event.delete()
+    if x==19:
+        await borg.send_file(event.chat_id,img19)
+        await event.delete()
+    if x==20:
+        await borg.send_file(event.chat_id,img20)
+        await event.delete()
+    if x==21:
+        await borg.send_file(event.chat_id,img21)
+        await event.delete()
+    if x==22:
+        await borg.send_file(event.chat_id,img22)
+        await event.delete()
+    if x==23:
+        await borg.send_file(event.chat_id,img23)
+        await event.delete()
+    if x==24:
+        await borg.send_file(event.chat_id,img24)
+        await event.delete()
+    if x==25:
+        await borg.send_file(event.chat_id,img25)
+        await event.delete()
+    if x==26:
+        await borg.send_file(event.chat_id,img26)
+        await event.delete()
+    if x==27:
+        await borg.send_file(event.chat_id,img27)
+        await event.delete()
+    if x==28:
+        await borg.send_file(event.chat_id,img28)
+        await event.delete()
+    if x==29:
+        await borg.send_file(event.chat_id,img29)
+        await event.delete()
+    if x==30:
+        await borg.send_file(event.chat_id,img30)
+        await event.delete()
+    if x==31:
+        await borg.send_file(event.chat_id,img31)
+        await event.delete()
+    if x==32:
+        await borg.send_file(event.chat_id,img32)
+        await event.delete()
+    if x==33:
+        await borg.send_file(event.chat_id,img33)
+        await event.delete()
+    if x==34:
+        await borg.send_file(event.chat_id,img34)
+        await event.delete()
+    if x==35:
+        await borg.send_file(event.chat_id,img35)
+        await event.delete()
+    if x==36:
+        await borg.send_file(event.chat_id,img36)
+        await event.delete()
+    if x==37:
+        await borg.send_file(event.chat_id,img37)
+        await event.delete()
+    if x==38:
+        await borg.send_file(event.chat_id,img38)
+        await event.delete()
+    if x==39:
+        await borg.send_file(event.chat_id,img39)
+        await event.delete()
+    if x==40:
+        await borg.send_file(event.chat_id,img40)
+        await event.delete()
+    # don't copy please😭
