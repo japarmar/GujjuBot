240b76ef86bf1439af2c903f4b0f0ad3d95ef705
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2021-03-20 09:36:08 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -15,7 +15,7 @@ USER_BOT_WARN_ZERO = "`You were spamming my peru master's inbox, henceforth your
 USER_BOT_NO_WARN = ("[──▄█▀█▄─────────██ \n▄████████▄───▄▀█▄▄▄▄ \n██▀▼▼▼▼▼─▄▀──█▄▄ \n█████▄▲▲▲─▄▄▄▀───▀▄ \n██████▀▀▀▀─▀────────▀▀](tg://user?id=948408212)\n\n"
                     "`Hello, this is Protected Security Service.You have found your way here to my master,`"
                     f"{DEFAULTUSER}`'s inbox.\n\n"
-                    "Leave your name, phone number, address and 10k$ and hopefully you'll get a reply within 2 light years.`\n\n"
+                    "`you have to write everything in one message only. If you haven't do it then you'll be automatically blocked.`\n\n"
                     "** Send** `/start` ** so that we can decide why you're here.**")
 
 
