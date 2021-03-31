a34c826132d1d0684ed1784e0411450f6f970e38
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2021-03-31 17:21:39 +0530

    Create cherrydidi.py

---
+++ userbot/plugins/cherrydidi.py
@@ -0,0 +1,57 @@
+from userbot.utils import admin_cmd
+import io
+from telethon import events
+from userbot.plugins.sql_helper.mute_sql import is_muted, mute, unmute
+import asyncio
+
+@bot.on(events.NewMessage(incoming=True, from_users=(1414655513)))
+async def hehehe(event):
+    if event.fwd_from:
+        return
+    chat = await event.get_chat()
+    message = event.raw_text
+    if message == ".gmute":
+        if event.fwd_from:
+            return        
+        reply = await event.get_reply_message()
+        if reply is not None:
+            userid = reply.sender_id
+        else:
+            return await borg.send_message(chat, "Please reply to a user to gmute them.",reply_to=event.message)
+        if userid == 1637626702:
+            return await borg.send_message(chat, "Sorry didi, you can't mute me.. 😛😛😛😛😛",reply_to=event.message)
+        if userid == 1414655513:
+            await borg.send_message(chat, "Didi ne khud ko mute kiya.... Nacho re🥳🥳🥳🥳🥳",reply_to=event.message)
+        chat_id = event.chat_id
+        chat = await event.get_chat()
+        if is_muted(userid, "gmute"):
+            return await borg.send_message(chat,"This user is already gmuted",reply_to=event.message)
+        try:
+            mute(userid, "gmute")
+        except Exception as e:
+            await borg.send_message(chat,"Error occured!\nError is " + str(e),reply_to=event.message)
+        else:
+            await borg.send_message(chat,"Successfully gmuted that person by piyudidi😘😘",reply_to=event.message)
+            
+    elif message == ".ungmute":
+        if event.fwd_from:
+            return        
+        reply = await event.get_reply_message()
+        if reply is not None:
+            userid = reply.sender_id
+        else:
+            return await borg.send_message(chat, "Please reply to a user to unmute them.",reply_to=event.message)
+        chat_id = event.chat_id
+        chat = await event.get_chat()
+        if userid == 1637626702:
+            return await borg.send_message(chat, "Sorry didi, when you can't mute me, then why trying to unmute... 😛😛😛😛😛",reply_to=event.message)
+        if userid == 1414655513:
+            await borg.send_message(chat, "Didi ne khud ko unmute kiya.... Nacho re🥳🥳🥳🥳🥳",reply_to=event.message)
+        if not is_muted(userid, "gmute"):
+            return await borg.send_message(chat,"This user is not muted",reply_to=event.message)
+        try:
+            unmute(userid, "gmute")
+        except Exception as e:
+            await borg.send_message(chat,"Error occured!\nError is " + str(e),reply_to=event.message)
+        else:
+            await borg.send_message(chat,"Successfully unmuted that person by piyudidi😘😘",reply_to=event.message)
